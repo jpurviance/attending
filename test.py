42b4517e5f158e30e24a36ead9f386df933690b7
John Purviance <jpurviance@vt.edu>
2019-05-09 12:01:55 -0400

    recover old work

---
+++ test.py
@@ -0,0 +1,14 @@
+import foobar
+import fizbuz
+from attending import Library
+
+library = Library()
+print(foobar in library)
+library.fetch(foobar)
+print(foobar in library)
+print(library[foobar])
+
+print(fizbuz in library)
+library.fetch(fizbuz)
+print(fizbuz in library)
+print(library[fizbuz])
